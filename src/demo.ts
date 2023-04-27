9728eb51486889b2d58bd88875e37cea8a65bd14
Jepmoltho <jept@itu.dk>
2023-04-27 14:19:33 +0200

    Exe 2.4 complete

--- src/demo.ts
+++ src/demo.ts
@@ -1,13 +1,21 @@
 interface Contact {
-    id: number;
-    name: ContactName;
-    birthDate?: Date;
+  id: number;
+  name: ContactName;
+  birthDate?: Date;
+  cuntrycode: number;
+}
+
+enum Countrycodes {
+  DK = 45,
+  UK = 0o0,
+  US = 0o1,
 }
 
 let primaryContact: Contact = {
-    birthDate: new Date("01-01-1980"),
-    id: 12345,
-    name: "Jamie Johnson",
-}
+  birthDate: new Date("01-01-1980"),
+  id: 12345,
+  name: "Jamie Johnson",
+  cuntrycode: Countrycodes.DK,
+};
 
-type ContactName = string
+type ContactName = string;
