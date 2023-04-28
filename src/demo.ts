7bd0dd93a28599a22c41759eef62ab50b7172070
Jepmoltho <jept@itu.dk>
2023-04-28 15:52:31 +0200

    Exe 02_05b complete

--- src/demo.ts
+++ src/demo.ts
@@ -1,8 +1,34 @@
+//Task: Only apply Contact type in param
+
 interface Contact {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
 
-function clone(source) {
-    return Object.apply({}, source);
+function returnContact(source: Contact): Contact {
+  return source;
 }
+
+function clone(source: Contact) {
+  return Object.apply({}, source);
+}
+
+function cloneTwo(
+  source: Contact,
+  func: (source: Contact) => Contact
+): Contact {
+  return Object.apply({}, source);
+}
+
+function myGenericFunction<T>(humlebuks: T): T {
+  return humlebuks;
+}
+
+myGenericFunction(true);
+myGenericFunction<string>("Hello");
+
+/*
+function printMyGenerics(){
+    console.log(myGenericFunction())
+}
+*/
