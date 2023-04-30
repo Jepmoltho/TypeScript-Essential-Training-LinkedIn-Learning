6554fd2d95af94c049fe5e29a519ab2ec64a4619
Jepmoltho <jept@itu.dk>
2023-04-30 12:19:09 +0200

    Challenge 4: Return

--- exercise_files/Ch04/04_Challenge/app.ts
+++ exercise_files/Ch04/04_Challenge/app.ts
@@ -1,7 +1,18 @@
-function query<T>(
-    items: T[],
-    query: any // <--- replace this!
-) {
+/*
+type Contact = {
+  name: Function;
+  age: Function;
+};
+
+type contactProperty = Record<keyof Contact, Function>;
+
+function matchesFun(arg: string): boolean {
+  return true;
+}
+*/
+//prettier-ignore
+function query<T>(items: T[], query: Record<keyof T, (val: T[keyof T]) => boolean>) : T[] /* <--- replace this!)*/  {  
+    //returns an array of all items matching the query
     return items.filter(item => {
         // iterate through each of the item's properties
         for (const property of Object.keys(item)) {
@@ -21,11 +32,15 @@ function query<T>(
 }
 
 const matches = query(
-    [
-        { name: "Ted", age: 12 },
-        { name: "Angie", age: 31 }
-    ],
-    {
-        name: name => name === "Angie",
-        age: age => age > 30
-    })
+  [
+    //Each of theese properties on this query object, should be a function that accepts the value of that property and returns true, if the value matches the function
+    { name: "Ted", age: 12 },
+    { name: "Angie", age: 31 },
+  ],
+  //The right type is this object: An object that allows for properties, that match the properties of the objects, in the array above it
+  {
+    //Sucess: When name returns string onHover
+    name: (name) => name === "Angie",
+    age: (age) => age > 30,
+  }
+);
